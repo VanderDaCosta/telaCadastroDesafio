62b6bd6d53a67d31be339bb708e21b4f9a4fb271
vander <opessoaldovander@gmail.com>
2022-12-19 22:30:05 -0300

    Tela Cadastro

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -40,26 +40,34 @@ const Login = () => {
         <Header />
         <Container>
             <Column>
-                <Title>A plataforma para você aprender com experts, dominar as principais tecnologias
-                 e entrar mais rápido nas empresas mais desejadas.</Title>
+                <Title>
+                    A plataforma para você aprender com experts, dominar as principais tecnologias e entrar mais rápido nas empresas mais desejadas.
+                </Title>
             </Column>
-            <Column>
+            <Row>
                 <Wrapper>
-                <TitleLogin>Faça seu cadastro</TitleLogin>
-                <SubtitleLogin>Faça seu login e make the change._</SubtitleLogin>
-                <form onSubmit={handleSubmit(onSubmit)}>
-                    <Input placeholder="E-mail" leftIcon={<MdEmail />} name="email"  control={control} />
-                    {errors.email && <span>E-mail é obrigatório</span>}
-                    <Input type="password" placeholder="Senha" leftIcon={<MdLock />}  name="senha" control={control} />
-                    {errors.senha && <span>Senha é obrigatório</span>}
-                    <Button title="Entrar" variant="secondary" type="submit"/>
-                </form>
-                <Row>
-                    <EsqueciText>Esqueci minha senha</EsqueciText>
-                    <CriarText>Criar Conta</CriarText>
-                </Row>
+                    <TitleLogin>Comece agora grátis</TitleLogin>
+                    <SubtitleLogin>Crie sua conta e make the change.</SubtitleLogin>
+                    <form onSubmit={handleSubmit(onSubmit)}>
+                        <Input placeholder="Nome Completo" leftIcon={<MdPerson />} name="nome" control={control} />
+                        <Input placeholder="Seu melhor @e-mail" leftIcon={<MdEmail />} name="email" control={control} />
+                        {errors.email && <span>E-mail é obrigatório</span>}
+                        <Input placeholder="Celular ex: (11) 96123-4567" leftIcon={<MdPhone />} name="email" control={control} />
+                        {errors.email && <span>E-mail é obrigatório</span>}
+                        <Input type="password" placeholder="Senha" leftIcon={<MdLock />} name="senha" control={control} />
+                        {errors.senha && <span>Senha é obrigatório</span>}
+                        <Button title="Criar minha conta" variant="secondary" type="submit" />
+                    </form>
+                    <Row>
+                        <SubtitleLogin>
+                            Ao clicar em "criar minha conta grátis", declaro que aceito as Políticas de Privacidade e os Termos de Uso da DIO.
+                            <Row>
+                            <JaPossuiConta>Já tenho conta. <CriarText onClick={handleClickSignIn}>Fazer login</CriarText></JaPossuiConta>
+                            </Row>
+                        </SubtitleLogin>
+                    </Row>
                 </Wrapper>
-            </Column>
+            </Row>
         </Container>
     </>)
 }
